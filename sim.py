ca5d356fcac22d7093fee1e9c67120d6dc80910c
Logan <loganparker0803@gmail.com>
2024-05-29 16:29:49 -0500

    Started the physics simulation and the plotting of such. Will need to be worked on.

---
+++ sim.py
@@ -0,0 +1,10 @@
+
+nop=1 #number of particles
+q=1     #particle charge
+m=1    #particle mass
+B=(0,0,1) #magnetic field
+E=(0,0,0) #electric field
+dt=0.01 #time step
+nt=1000 #number of time steps -> actual time = nt*dt=10
+x0=(0,0,0) #initial particle position
+v0=(1,0,0) #initial particle velocity
