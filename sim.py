e7e903a79e0857496f1a7bb7a364158cc1e8c894
Logan <loganparker0803@gmail.com>
2024-05-30 11:27:16 -0500

    Plotting Update Changed plotting from XY to 3D and 3 plots for the orthogonal views (XY, XZ, and YZ).

--- sim.py
+++ sim.py
@@ -12,7 +12,7 @@ B = np.array([0.0, 0.0, 1.0])           # magnetic field
 dt = 0.01                               # time step
 nt = 1000                               # number of time steps
 x = np.array([0.0, 0.0, 0.0])           # initial particle position
-v = np.array([1.0, 0.0, 0.0])           # initial particle velocity
+v = np.array([1.0, 0.0, 1.0])           # initial particle velocity
 
 pos = np.zeros((nt, 3))                 # array of positions
 vel = np.zeros((nt, 3))                 # array of velocities
@@ -37,16 +37,40 @@ for step in range(nt):
     # Full-step for position.
     x = x + v * dt
 
-# Graph only concerned with X and Y values.
-xpos = [p[0] for p in pos]
-ypos = [p[1] for p in pos]
-
 # Plotting
-fig = plt.figure()
-ax = fig.add_subplot()
-ax.plot(xpos, ypos)
-ax.set_xlabel('X')
-ax.set_ylabel('Y')
-ax.set_box_aspect(1) # Graph is 1:1
-ax.set_title("Particle Trajectory")
+fig = plt.figure(figsize=(10, 8))
+
+# 3D Plot
+ax1 = fig.add_subplot(221, projection='3d')
+ax1.plot(pos[:, 0], pos[:, 1], pos[:,2])
+ax1.set_xlabel('X')
+ax1.set_ylabel('Y')
+ax1.set_zlabel('Z')
+ax1.set_title('3D Trajectory')
+
+# XY Plot
+ax2 = fig.add_subplot(222)
+ax2.plot(pos[:, 0], pos[:, 1])
+ax2.set_xlabel('X')
+ax2.set_ylabel('Y')
+ax2.set_title('XY')
+ax2.set_box_aspect(1)
+
+# XZ Plot
+ax3 = fig.add_subplot(223)
+ax3.plot(pos[:, 0], pos[:, 2])
+ax3.set_xlabel('X')
+ax3.set_ylabel('Z')
+ax3.set_title('XZ')
+ax3.set_box_aspect(1)
+
+# YZ Plot
+ax4 = fig.add_subplot(224)
+ax4.plot(pos[:, 1], pos[:, 2])
+ax4.set_xlabel('Y')
+ax4.set_ylabel('Z')
+ax4.set_title('YZ')
+ax4.set_box_aspect(1)
+
+plt.tight_layout()
 plt.show()
